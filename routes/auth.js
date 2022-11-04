18ed03e35d96fa65f0e02371152249929d66a07b
jtompkins415 <john.tompkins415@gmail.com>
2022-11-04 08:36:46 -0700

    write POST routes for user login and registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,30 @@
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("../expressError");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+const User = require('../models/user')
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+router.post('/login', async (req, res, next) => {
+    try{
+        const {username, password} = req.body
 
+        if(await User.authenticate(username, password)){
+            User.updateLoginTimestamp(username)
+            const token = jwt.sign({username}, SECRET_KEY)
+            return res.json({token})
+        }else {
+            throw new ExpressError('Invalid Username/Password', 404)
+        }
+    }catch(e){
+        return next(e)
+    }
+})
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +32,17 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post('/register', async (req, res, next) => {
+    try{
+        const {username} = await User.register(req.body)
+        const token = jwt.sign({username}, SECRET_KEY)
+        User.updateLoginTimestamp(username)
+        return res.json({token})
+
+    } catch(e) {
+        return next(e)
+    }
+})
+
+module.exports = router;
