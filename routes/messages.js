90b63cc67eb700b39088bb2bb112c3cff4f9c58a
jtompkins415 <john.tompkins415@gmail.com>
2022-11-04 10:49:34 -0700

    write GET route for viewing messages, write POST route for message creation and marking read

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("../expressError");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY, DB_URI } = require("../config");
+const User = require('../models/user');
+const Message = require('../models/message');
+const {authenticateJWT, ensureCorrectUser, ensureLoggedIn} = require('../middleware/auth');
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -11,6 +20,22 @@
  *
  **/
 
+router.get('/:id', ensureLoggedIn, async (req, res, next) => {
+    try{
+        const username = req.user.username
+        const message = await Message.get(req.params.id)
+
+        if(message.to_user.username !== username && message.from_user.username !== username){
+            throw new ExpressError('Invalid User', 401);
+        }
+        return res.json({message: message})
+
+    }catch(e){
+        return next(e)
+    }
+})
+
+
 
 /** POST / - post message.
  *
@@ -19,6 +44,20 @@
  *
  **/
 
+router.post('/', ensureLoggedIn, async (req, res, next) => {
+    try{
+        const message = await Message.create({
+            from_username: req.user.username,
+            to_username: req.body.to_username,
+            body: req.body.body
+        })
+
+        return res.json({message: message})
+
+    }catch(e){
+        return next(e)
+    }
+})
 
 /** POST/:id/read - mark message as read:
  *
@@ -27,4 +66,23 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+
+router.post('/:id/read', ensureLoggedIn, async (req, res, next) => {
+    try{
+        const username = req.user.username;
+        const message = await Message.get(req.params.id);
+
+        if(message.to_user.username !== username){
+            throw new ExpressError('Invalid Recipient', 401);
+        }
+
+        const msg = await Message.markRead(req.params.id)
+
+        return res.json({msg})
+    }catch(e){
+        return next(e)
+    }
+});
+
+module.exports = router;
 
